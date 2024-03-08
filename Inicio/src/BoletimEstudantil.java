845de245e259746677d72b214066d7b7e7b86f67
Arthur Ávila <arthur_ti@outlook.com>
2024-03-08 16:30:03 -0300

    as

---
+++ Inicio/src/BoletimEstudantil.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class BoletimEstudantil {
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Informe sua Primeira Nota ");
+        double nota1=sc.nextDouble();
+        System.out.print("Informe sua Segunda Nota  ");
+        double nota2=sc.nextDouble();
+        double media=(nota1+nota2)/2;
+        
+        if(media <4){
+            System.out.println("Aluno Reprovado ");
+            System.out.println(media);
+        }
+        else if(media >4 && media <=6){
+            System.out.println("Aluno em Recuperação");
+            System.out.println(media);
+        }
+        else{
+            System.out.println("Aluno Aprovado");
+            System.out.println(media);
+        }
+        
+
+    }
+    
+}
--- Inicio/src/ChamadaMetodo.java
+++ Inicio/src/ChamadaMetodo.java
@@ -1,17 +1,18 @@
 public class ChamadaMetodo {
 
-    public static void main(String[] args){
-         String primeiroNome="Grupo";
-         String segundoNome="Dislub";
+    public static void main(String[] args) {
+        String primeiroNome = "Grupo";
+        String segundoNome = "Dislub";
 
-               //Chamada do Metodo    
-         String nomeCompleto= nomeCompleto(primeiroNome, segundoNome);
-         System.out.println(nomeCompleto);
+        // Chamada do Metodo
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
     }
-              // Atribuição do Metodo
-    public static String nomeCompleto(String primeiroNome,String segundoNome){
-        return primeiroNome.concat(" ").concat(segundoNome);
+
+    // Atribuição do Metodo
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado da concatenação: " + primeiroNome.concat(" ").concat(segundoNome);
 
     }
-    
+
 }
